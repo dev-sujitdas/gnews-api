5cf45d42d244e0983c3413943977d65311b58d37
dev-sujitdas <dev.sujitdas@gmail.com>
2025-06-25 21:28:28 +0530

    news api first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# News API Server for GNews 
---
+++ api/news.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const apiKey = process.env.GNEWS_API_KEY;
+  const api = `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=${apiKey}`;
+
+  
+  if (global.cachedNews && Date.now() - global.lastFetchTime < 15 * 60 * 1000) {
+    return res.status(200).json(global.cachedNews);
+  }
+
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+
+    global.cachedNews = data;
+    global.lastFetchTime = Date.now();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
