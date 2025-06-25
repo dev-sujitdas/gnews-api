942df9c277b4db333e922a787603464a7fe47777
dev-sujitdas <dev.sujitdas@gmail.com>
2025-06-25 21:51:24 +0530

    cors added to the api

--- api/news.js
+++ api/news.js
@@ -1,7 +1,15 @@
-export default async function handler(req, res) {
+export default async function handler(req, res) {  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  
   const apiKey = process.env.GNEWS_API_KEY;
   const api = `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=${apiKey}`;
-
   
   if (global.cachedNews && Date.now() - global.lastFetchTime < 15 * 60 * 1000) {
     return res.status(200).json(global.cachedNews);
